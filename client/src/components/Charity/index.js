ce73c76d12f96172f78e898c1b5532c8ab301005
Cecilia Leon <leoncecilia17@gmail.com>
2019-09-28 15:04:45 -0500

    set up charity card

---
+++ client/src/components/Charity/index.js
@@ -0,0 +1,18 @@
+import React from "react"; 
+import "./style.css"; 
+
+function Charity({liked, handleLike, id, name, image, description, website}){
+    return(
+        <div className= "charityapp">
+            <h5 onClick={() => handleLike(id)}>{liked ? 'site is liked' : 'site is not liked'}</h5>
+            <p>{id}</p>
+            <h1>{name}</h1>
+            <p>{image}</p>
+            <p>{description}</p>
+            <h3>{website}</h3>
+            <button>Donate Now</button>
+        </div>
+    ); 
+}
+
+export default Charity; 
