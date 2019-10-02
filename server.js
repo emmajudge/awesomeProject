6b49d8c653c319cbe062a701410bec25d5a78a46
Mari0203 <mari.brands@pm.me>
2019-10-01 21:00:15 -0500

    Updated server.js with routes

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/awesomeProject");
 
+// "mongodb://test:password1@ds023714.mlab.com:23714/heroku_d20xtg1f"
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
